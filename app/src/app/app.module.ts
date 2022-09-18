3c1ed89566898ecdd263c56478f72fb2f85ff901
Vivek8210 <99641318+Vivek8210@users.noreply.github.com>
2022-09-18 21:59:13 +0530

    completed

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
-    DialogBoxComponent
+   
   ],
   imports: [
     BrowserModule,
